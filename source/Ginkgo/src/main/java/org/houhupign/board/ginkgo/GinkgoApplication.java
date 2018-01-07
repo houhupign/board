29bbc0077a5b239d549479974d04c074c2b68192
houhupign <houhupign@126.com>
2018-01-08 00:24:25 +0800

    init

---
+++ source/Ginkgo/src/main/java/org/houhupign/board/ginkgo/GinkgoApplication.java
@@ -0,0 +1,12 @@
+package org.houhupign.board.ginkgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinkgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinkgoApplication.class, args);
+	}
+}
